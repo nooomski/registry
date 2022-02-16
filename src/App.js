412bef6ae0570ed0ec29a30a8a2c7f10dc0409f6
Noam Cohen <81436914+nooomski@users.noreply.github.com>
2022-02-16 02:59:31 +0100

    more field parsing

--- src/App.js
+++ src/App.js
@@ -7,41 +7,52 @@ import ChainData from './components/ChainData';
 
 function App() {
   const [data, setData] = useState([]);
-  const [currentChain, setCurrentchain] = useState([]);
+  const [currentChainData, setCurrentChainData] = useState([]);
   const defaultChain = "cosmoshub";
+  const [currentChain, setCurrentChain] = useState(defaultChain);
 
   useEffect(() => {
+    // Get folder structure from repo
     let folderUrl = 'https://api.github.com/repos/cosmos/chain-registry/contents';
     axios.get(folderUrl).then((res) => {
+      // Filter only relevant
       const result = res.data.filter((item) => item.type === 'dir' && item.name !== '.github');
       setData(result);
     });
+    
+    // Fetch the first chain
+    fetchChainData();
   }, []);
 
-  const handleChange = (e) => {
-    fetchChainData(e.target.value);
-  };
+  const fetchChainData = (e) => {
+    let v = (e) ? e.target.value : defaultChain;
 
-  const fetchChainData = (v) => {
+    // Set URL
     let fieldUrl =
       'https://cdn.jsdelivr.net/gh/cosmos/chain-registry@master/' + v + '/chain.json';
 
+    // Get chain.json file and filter out irrelevant data
     axios.get(fieldUrl).then((res) => {
       let result = { ...res.data };
       delete result.$schema;
-      setCurrentchain(result);
+      setCurrentChain(result.chain_name);
+      setCurrentChainData(result);
     });
   }
 
+  const addToKeplr = e => {
+
+  }
+  
   return (
-    <div className='app'>
+    <div className='app bg-gray-700'>
       <header className='flex items-center justify-between px-5 bg-gray-800 h-14'>
-        <h1 className='text-xl font-bold tracking-wider text-white'>Chain Registry</h1>
+        <h1 className='text-xl font-bold tracking-wider text-gray-100'>Chain Registry</h1>
         <div>
-          <label htmlFor='blockchains' className='mr-4 font-bold text-white'>
+          <label htmlFor='blockchains' className='mr-4 font-bold text-gray-100'>
             Select Chain:
           </label>
-          <select className='capitalize' id='blockchains' onChange={handleChange} value={defaultChain}>
+          <select className='capitalize' id='blockchains' onChange={fetchChainData} value={currentChain}>
             {data.map((chain) => {
               return (
                 <option key={chain.name} value={chain.name}>
@@ -49,12 +60,12 @@ function App() {
                 </option>
                 );
             })}
-            {fetchChainData(defaultChain)}
 
           </select>
+          <button className='bg-gray-600 hover:bg-gray-700 text-gray-100 font-bold py-1 px-4 ml-4 rounded' id='keplrAdd' onClick={addToKeplr}>Add to Keplr</button>
         </div>
       </header>
-      <ChainData currentChain={currentChain} />
+      <ChainData currentChainData={currentChainData} />
     </div>
   );
 }
--- src/components/ChainData.jsx
+++ src/components/ChainData.jsx
@@ -1,22 +1,134 @@
 import { stringify } from 'flatted';
 import React from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
-const ChainData = ({ currentChain }) => {
-  /* const keys = Object.keys(currentChain);
-  //let val = Object.values(currentChain);
+const renderValue = (ccd) => {
+  let val = JSON.stringify(ccd).replace(/[{",}]/g, '')
+  console.log(val);
+
+  switch(true) {
+    case (val == 'live'):
+      return (
+        <div className="capitalize">
+          {val}
+             <span className="mx-1 relative inline-flex rounded-full h-3 w-3 bg-emerald-500"><span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"></span></span>
+        </div>
+      );
+    case ((/^genesis_url/).test(val)):
+      return (
+        <div>
+          <div className="px-2 py-1 bg-gray-800 text-gray-300 font-mono rounded-md">
+            <a href={ccd.genesis_url} target="_blank" className="hover:bg-emerald-800">
+              {ccd.genesis_url}
+            </a>
+          </div>
+        </div>
+      );
+    case ((/^git_repo/).test(val)):
+      return (
+        <div>
+          <label className="px-2">GitHub Repo:</label>
+          <div className="my-1 mb-5 px-2 py-1 bg-gray-800 text-gray-300 font-mono rounded-md">
+            <a href={ccd.git_repo} target="_blank" className="hover:bg-emerald-800">
+              {ccd.git_repo}
+            </a>
+          </div>
+          <label className="px-2">Binaries:</label>
+          <div className="my-1 px-2 py-1 bg-gray-800 text-gray-300 font-mono rounded-md">
+            {Object.keys(ccd.binaries).map((el) => (
+              <div>{el}:&nbsp;
+                <a href={String(ccd.binaries[el])} target="_blank" className="hover:bg-emerald-800">
+                  {String(ccd.binaries[el])}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      );//{JSON.stringify(ccd.binaries)}
+    case ((/^seeds/).test(val)):
+      return (
+        <div>
+          <label className="px-2">Seeds:</label>
+          <div className="my-1 mb-5 px-2 py-1 bg-gray-800 text-gray-300 font-mono rounded-md">
+            {Object.keys(ccd.seeds).map((el) => (
+              <div className="inline-block">{String(ccd.seeds[el].address)},&nbsp;
+              </div>
+            ))}
+          </div>
+          <label className="px-2">Persistent Peers:</label>
+          <div className="my-1 px-2 py-1 bg-gray-800 text-gray-300 font-mono rounded-md">
+            {Object.keys(ccd.persistent_peers).map((el) => (
+              <div className="inline-block">{String(ccd.persistent_peers[el].address)},&nbsp;
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+    default:
+      return (
+        <div>
+          {val}
+        </div>
+      );
+  }
+}
+
+const ChainData = ({ currentChainData }) => {
+  const keys = Object.keys(currentChainData);
+  let val;
+
   return (
     <div>
       {keys.map((el, idx) => {
-        console.log(String(el));
-        return <h1 key={idx}>{el}: {currentChain[0]}</h1>;
+        console.log(idx + " " + String(el) + ": " + String(currentChainData[el]));
+        //return <h1 key={idx}>{el}: {String(currentChainData[el])}</h1>;
       })}
+
+      <div className="flex flex-col">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-900">
+              <table className="min-w-full divide-y divide-gray-900">
+                <thead className="bg-gray-700">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider"
+                    >
+                      Field
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider"
+                    >
+                      Value
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-gray-700 divide-y divide-gray-600">
+                  {keys.map((el, idx) => (
+                    <tr key={idx} className="hover:bg-cyan-900">
+                      <td className="px-6 py-4 w-1/6">
+                        <div className="text-sm font-medium text-gray-200 capitalize">{el.replace("_"," ")}</div>
+                      </td>
+                      <td className="px-6 py-4 w-5/6 break-all">
+                      <div className="text-sm text-gray-200 ">{renderValue(currentChainData[el])}</div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  ); */
+  );
   
   /* return (
     <div>
-      {currentChain.length === undefined ? (
-        <pre>{JSON.stringify(currentChain, undefined, 2).replace(/[{",}]/g, '')}</pre>
+      {currentChainData.length === undefined ? (
+        <pre>{JSON.stringify(currentChainData, undefined, 2).replace(/[{",}]/g, '')}</pre>
       ) : (
         <h1 className='m-5 text-xl font-bold'>Nothing selected</h1>
       )}
